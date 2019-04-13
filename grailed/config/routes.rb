c93e331360cdca15df34ebf86f82c48ec5fd979b
IsraelCea <ivcea@miuandes.cl>
2019-04-12 23:03:55 -0400

    Scaffold Users, Post, FlagPost, Comments, Votes

--- grailed/config/routes.rb
+++ grailed/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :flag_pos
+  resources :votes
+  resources :users
+  resources :comments
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- grailed/db/schema.rb
+++ grailed/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_13_013830) do
+ActiveRecord::Schema.define(version: 2019_04_13_025548) do
 
   create_table "blacklists", force: :cascade do |t|
     t.integer "user_id"
@@ -37,6 +37,14 @@ ActiveRecord::Schema.define(version: 2019_04_13_013830) do
     t.datetime "updated_at", null: false
     t.index ["autor_id"], name: "index_dumpsters_on_autor_id"
     t.index ["post_id"], name: "index_dumpsters_on_post_id"
+  end
+
+  create_table "flag_pos", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "reason"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flag_pos_on_user_id"
   end
 
   create_table "flag_posts", force: :cascade do |t|
