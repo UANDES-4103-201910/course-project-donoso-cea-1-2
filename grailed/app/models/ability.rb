2b7134f356b08a32cea56bf9d3c1594fe24408bd
johnny <jadonoso2@miuandes.cl>
2019-06-09 23:06:20 -0400

    can can op

--- grailed/app/models/ability.rb
+++ grailed/app/models/ability.rb
@@ -4,9 +4,10 @@ class Ability
   
   def initialize(user)
     user ||= User.new # guest user
+    can :manage, :all if user.role == "admin"
     can :manage, User, id: user.id
     can :read, :all
-    can :manage, Post, user_id: user.id
+    can :manage, Post, user_id: user.id || user.role == "admin"
   end
   
 end
